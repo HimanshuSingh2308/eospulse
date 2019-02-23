c0258f646ab802807847ea8108dfcfbab4fdabf9
himanshusingh2308 <himanshu.singh@koinex.in>
2019-02-23 20:50:35 +0530

    initial push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { YoutubePlayerModule } from 'ngx-youtube-player';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    YoutubePlayerModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit ,HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+
+
+export class LandingPageComponent implements OnInit {
+  video_id ='tQhXd47VVGU';
+  showYoutube = false;
+  ladda = false;
+  isMobile :Boolean;
+  innerWidth : any;
+  parseInt = parseInt;
+  constructor() { }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    if(parseInt(this.innerWidth) <= 600) {
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+    }
+    console.log(this.innerWidth,"resize");
+  }
+
+  ngOnInit() {
+    if( window.innerWidth <= 600 ) {
+      this.isMobile = true;
+    } else {
+      this.isMobile =false;
+    }
+  }
+
+  savePlayer(event){
+    console.log("ready ",event);
+    this.ladda= false;
+    event.playVideo();
+  }
+
+  playYoutubeVideo(){
+    this.ladda = true;
+    this.showYoutube = true;
+  }
+
+  onStateChange(event){
+    console.log("state change ",event);
+    if(event.data == 0) {
+      event.target.stopVideo();
+      this.showYoutube = false;
+    }
+  }
+
+}
